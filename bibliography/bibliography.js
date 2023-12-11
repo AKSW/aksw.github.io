16f12097a42a7565e85ad488bd9867dece73b07a
Jonas <jonasgwozdz@rocketmail.com>
2023-12-11 13:55:48 +0100

    bootstrap removed, style fix, toggle fix

--- bibliography/bibliography.js
+++ bibliography/bibliography.js
@@ -173,7 +173,7 @@ function toggleAuthorFilter() {
 // Toggles the display of a filter
 function toggleFilterDisplay(filterId) {
   var filter = document.getElementById(filterId);
-  if (filter.style.display === "none") {
+  if (!filter.style.display || filter.style.display === "none") {
     filter.style.display = "block";
   } else {
     filter.style.display = "none";
